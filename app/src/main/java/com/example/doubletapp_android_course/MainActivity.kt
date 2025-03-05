dab54dfdbfb765f04f2725da65a5b30f02f8c566
Nihat04 <aliev.nihat4@gmail.com>
2025-03-05 15:26:44 +0500

    Initial commit

---
+++ app/src/main/java/com/example/doubletapp_android_course/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.doubletapp_android_course
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
