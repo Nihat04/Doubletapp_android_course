01a7aaeeea206ee2504a89aea8e8427e50358017
Nihat04 <aliev.nihat4@gmail.com>
2025-04-03 21:43:27 +0500

    remove filterBottomSheetGragment

--- app/src/main/java/com/example/doubletapp_android_course/ui/fragments/FilterBottomSheetFragment.kt
+++
@@ -1,5 +0,0 @@
-package com.example.doubletapp_android_course.ui.fragments
-
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-
-class FilterBottomSheetFragment : BottomSheetDialogFragment() {}
